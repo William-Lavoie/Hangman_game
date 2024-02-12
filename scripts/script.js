abc61300abec94383c21af209c826de1b7ad255c
William-Lavoie <william.lavoie.3@ens.etsmtl.ca>
2024-02-12 14:58:09 -0500

    Creating divs dynamically and capturing keyboard inputs

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,32 @@
-document.onready(function() {
+$(document).ready(function() {
 
-    const WORDS = ["word" , "test"];
+    const WORDS = ["word" , "a", "ab"];
     let wordGame = "";
     let counter = 0;
     let mistakes = 0;
 
-    $("#new-game").onclick(function() {
-        let random = Math.floor(Math.random()*WORDS.length +1);
+    $("#new-game").click(function() {
+        let random = Math.floor(Math.random()*WORDS.length );
         wordGame = WORDS[random];
         counter = 0;
         mistakes = 0;
+        $("#container").html("");
 
         for (let i = 0; i < wordGame.length ; i++) {
             let newElement = $("<div></div>");
             newElement.addClass("letter");
+            $("#container").append(newElement);
         }
 
     })
     
-    document.on("keydown", function(event) {
+    $(document).on("keydown", function(event) {
 
         let letter = event.which;
+        console.log(event.which);
         if (letter == wordGame[counter]) {
             counter++;
+            $("#container").children()[counter].text(wordGame(counter-1));
         }
     })
 })
